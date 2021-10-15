ea64dae7ce27cd8b426d535241357d8e88a64fb6
Donald Szarkowicz <donald.szarkowicz@navy.mil>
2021-10-15 05:52:55 -0400

    Update from 10/15/21.

--- src/com/camprugged/Main.java
+++ src/com/camprugged/Main.java
@@ -3,7 +3,7 @@ package com.camprugged;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello, World!");
 
         System.out.println("Modified using Dell Desktop.");
